ee217d06695149194ee189634b781f6d5d537a8d
Adebayo Mohammed Olusiji <adebayomuhammad47@gmail.com>
2020-09-17 19:14:41 -0700

    feat: Implementing UI component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import NewTweet from "./NewTweet";
+import LoadingBar from "react-redux-loading";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
-  }
+	componentDidMount() {
+		this.props.dispatch(handleInitialData());
+	}
+	render() {
+		return (
+			<div>
+				<LoadingBar />
+				{this.props.loading === true ? null : <NewTweet />}
+			</div>
+		);
+	}
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+	return {
+		loading: authedUser === null,
+	};
+}
+
+export default connect(mapStateToProps)(App);
