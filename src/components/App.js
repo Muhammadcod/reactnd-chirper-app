256c2fb751897056bd7334434ae9221e5942a812
Adebayo Mohammed Olusiji <adebayomuhammad47@gmail.com>
2020-09-17 23:18:14 -0700

    feat: Implementing UI components

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
 import LoadingBar from "react-redux-loading";
 
 class App extends Component {
@@ -13,7 +14,11 @@ class App extends Component {
 		return (
 			<div>
 				<LoadingBar />
-				{this.props.loading === true ? null : <NewTweet />}
+				{this.props.loading === true ? null : (
+					<TweetPage
+						match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }}
+					/>
+				)}
 			</div>
 		);
 	}
--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS, TOGGLE_TWEET } from "../actions/tweets";
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from "../actions/tweets";
 
 export default function tweets(state = {}, action) {
 	switch (action.type) {
@@ -7,6 +7,7 @@ export default function tweets(state = {}, action) {
 				...state,
 				...action.tweets,
 			};
+
 		case TOGGLE_TWEET:
 			return {
 				...state,
@@ -21,6 +22,26 @@ export default function tweets(state = {}, action) {
 									action.authedUser,
 							  ]),
 				},
+			};
+		case ADD_TWEET:
+			const { tweet } = action;
+
+			let replyingTo = {};
+			if (tweet.replyingTo !== null) {
+				replyingTo = {
+					[tweet.replyingTo]: {
+						...state[tweet.replyingTo],
+						replies: state[tweet.replyingTo].replies.concat([
+							tweet.id,
+						]),
+					},
+				};
+			}
+
+			return {
+				...state,
+				[action.tweet.id]: action.tweet,
+				...replyingTo,
 			};
 		default:
 			return state;
